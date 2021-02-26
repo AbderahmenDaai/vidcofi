8300e3cb818acfe7602b5ed9978d567279a2c247
AbderahmenDaai <abderahmendaai@gmail.com>
2021-02-26 09:35:30 +0100

    Initial commit

---
+++ src/main/java/com/pfa/vidcofi/VidcofiApplication.java
@@ -0,0 +1,40 @@
+package com.pfa.vidcofi;
+
+import com.pfa.vidcofi.domain.Role;
+import com.pfa.vidcofi.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class VidcofiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidcofiApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+
+		return args -> {
+
+			Role adminRole = roleRepository.findByRole("ADMIN");
+			if (adminRole == null) {
+				Role newAdminRole = new Role();
+				newAdminRole.setRole("ADMIN");
+				roleRepository.save(newAdminRole);
+			}
+
+			Role userRole = roleRepository.findByRole("USER");
+			if (userRole == null) {
+				Role newUserRole = new Role();
+				newUserRole.setRole("USER");
+				roleRepository.save(newUserRole);
+			}
+		};
+
+	}
+
+}
---
+++ src/test/java/com/pfa/vidcofi/VidcofiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.vidcofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidcofiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
